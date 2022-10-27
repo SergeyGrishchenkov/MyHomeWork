fc8c16fa907442768378f3f3e1b4cd92f611c5b9
SergeyGrishchenkov <grisv1967@gmail.com>
2022-10-27 21:08:32 +0300

    test connecting gitHub

---
+++ Lesson_2/test2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 26 22:29:21 2022
+
+@author: Sergey Grishchenkov
+"""
+
+#example 1, just use print
+print('This is PHYTON')
+
+#example 2,  using multiple parameters
+print('This is PHYTON.', 'I use it to study programming', ' in', 2022, 'year')
+
+#example 3,  using add arguments
+print('This is PHYTON.', 'I use it to study programming', ' in', 2022, 'year', sep='...')
+print('This is PHYTON.', 'I use it to study programming', ' in', 2022, 'year', sep='...', end='!'*3)
