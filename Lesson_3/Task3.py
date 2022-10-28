8a11ade709ebb634fd4ede372e10181c773971d5
SergeyGrishchenkov <grisv1967@gmail.com>
2022-10-28 22:58:05 +0300

    Lesson 3 Home tasks

---
+++ Lesson_3/Task3.py
@@ -0,0 +1,11 @@
+a = 11
+
+b = 4
+
+print(f'{a} + {b} = {a + b}')
+print(f'{a} - {b} = {a - b}')
+print(f'{a} * {b} = {a * b}')
+print(f'{a} / {b} = {a / b}')
+print(f'{a} ** {b} = {a ** b}')
+print(f'{a} // {b} = {a // b}')
+print(f'{a} % {b} = {a % b}')
