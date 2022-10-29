d7b47ffc58ee3496f4938c6e79a443a2ee62a330
SergeyGrishchenkov <grisv1967@gmail.com>
2022-10-30 00:38:37 +0300

    Lesson 4 Home tasks

--- Lesson_4/Task3.py
+++ Lesson_4/Task3.py
@@ -5,4 +5,22 @@
 var1 = 4
 var2 = 3
 
+answer = 1
+
+mask_question = 'Input your answer for expression '
+
+output_mask_wrong = 'You answer is wrong!'
+output_mask_ok = 'You answer is correct.'
+
+
 list_expressions = ['+', '-', '*', '/', '**']
+
+for item in list_expressions:
+    print(f'{mask_question}{var1} {item} {var2}')
+    if item == '+' and answer == var1 + var2:
+        print(f'{answer}  {output_mask_ok}')
+    elif item == '-' and answer == var1 - var2:
+        print(f'{answer}  {output_mask_ok}')
+    else:
+        print(f'{answer}  {output_mask_wrong}')
+    # elif item == '+':
