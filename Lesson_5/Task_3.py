8dce0eab52336ed4957d803b694c2dccc3d074b7
SergeyGrishchenkov <grisv1967@gmail.com>
2022-11-01 19:54:16 +0300

    Lesson 45 Home tasks

--- Lesson_5/Task_3.py
+++ Lesson_5/Task_3.py
@@ -1,11 +1,2 @@
-import random
 
-greeting = 'Hello, do you want to play? You need to guess what the number is. Type Y or N'
-greeting_answer = input(greeting)
-if greeting_answer == 'Y':
-    player_name = input('Input your name:  ')
-elif greeting_answer == 'N':
-    print('See you next time!')
-else:
-    print('You have inputed wrong item. Would you like to try again?')
 
