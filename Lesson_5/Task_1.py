a6048fa400d91528f92251c7a3256ad0b1f10a58
SergeyGrishchenkov <grisv1967@gmail.com>
2022-11-02 20:38:28 +0300

    Lesson 5 Home tasks

--- Lesson_5/Task_1.py
+++ Lesson_5/Task_1.py
@@ -1,10 +1,35 @@
-import random
+from random import randint
 
-greeting = 'Hello, do you want to play? You need to guess what the number is. Type Y or N'
+#Report's masks
+
+greeting        = 'Hello, do you want to play? You need to guess what the number is. Type Y or N:\n'
+suggestion      = 'We have just chosen a number from 1 to 10. \nYou should guess, what the number is! \nInput a number: \n:'
+bad_input       = 'You should select a number from 1 to 10!\nSorry, you  lost the game!\n'
+bad_choice      = 'You choice is BAD!\nSorry, You  lost the game!\n'
+good_choice     = 'Congratulations!!! You have done THE RIGHT CHOICE!\n'
+
+
+def choice_random_int():
+    rand_int = randint(1, 10)
+    user_choice = input(suggestion)
+    if not user_choice.isdigit():
+        return [bad_input, -1, False]
+    elif int(user_choice) > 10 and int(user_choice) < 0:
+        return [bad_input, -1, False]
+    else:
+        if int(user_choice) == rand_int:
+            return [good_choice, user_choice, True]
+        else:
+            return [bad_choice, rand_int, False]
+
+
 greeting_answer = input(greeting)
 if greeting_answer == 'Y':
     player_name = input('Input your name:  ')
+    result = choice_random_int()
+    add_report = '' if result[2] else ('Correct choice is ' + str(result[1]))
+    print(f'{player_name}, {result[0]} {add_report}')
 elif greeting_answer == 'N':
-    print('See you next time!')
+    print(f'See you next time!')
 else:
-    print('You have inputed wrong item. Would you like to try again?')
+    print(f'You have inputed wrong item.')
--- Practic/Lesson_5_practic.py
+++ Practic/Lesson_5_practic.py
@@ -1,1 +1,16 @@
-input('tell me smth:  ')
+# print("before import")
+import math
+
+print("before function_a")
+def function_a():
+    print("Function A")
+
+print("before function_b")
+def function_b():
+    print("Function B {}".format(math.sqrt(100)))
+
+print("before __name__ guard")
+if __name__ == '__main__':
+    function_a()
+    function_b()
+print("after __name__ guard")
