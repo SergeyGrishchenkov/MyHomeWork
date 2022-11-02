48fe65ada6016ca23b72327f2511cc29484bbc43
SergeyGrishchenkov <grisv1967@gmail.com>
2022-11-02 22:10:51 +0300

    Lesson 5 Home tasks

--- Lesson_5/Task_2.py
+++ Lesson_5/Task_2.py
@@ -6,12 +6,13 @@
 
 user_name = input('Input your first name:\n')
 user_age = input('Input your age, range from 5 to 99:\n')
-wrong_user_age = input('You inputed wrong age.\n')
+wrong_user_age = 'You inputed wrong age.\n'
 
-#проверяем возраст на корректность
-if not user_age.isdigit() or int(user_age) < 5 or int(user_age) > 99:
-    print(f'{user_name} {wrong_user_age}')
-else:
-    next_age = int(user_age)
-    print(f'Hello {user_name}, on your next birthday you’ll be {next_age} years.\n')
+if __name__ == "__main__":
+    #проверяем возраст на корректность
+    if not user_age.isdigit() or int(user_age) < 5 or int(user_age) > 99:
+        print(f'{user_name} {wrong_user_age}')
+    else:
+        next_age = int(user_age) + 1
+        print(f'Hello {user_name}, on your next birthday you’ll be {next_age} years.\n')
 
