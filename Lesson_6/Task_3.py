4aae5c01ba068ab829ee2647770399a4cd94f3be
grisv <grisv1967@gmail.com>
2022-11-04 10:15:31 +0300

    Lesson 6 Home tasks

---
+++ Lesson_6/Task_3.py
@@ -0,0 +1,5 @@
+# The greatest number
+#
+# Write a Python program to get the largest number from a list of random numbers with the length of 10
+#
+# Constraints: use only while loop and random module to generate numbers
