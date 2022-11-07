7f6c4cc93f3e5f2d0c7ec033713698adceac0d21
grisv <grisv1967@gmail.com>
2022-11-07 20:47:11 +0300

    Lesson 7 Home tasks

--- lesson_7/Task_4.py
+++ lesson_7/Task_4.py
@@ -1,3 +1,13 @@
 # Створити лист із днями тижня.
 # В один рядок (ну або як завжди) створити словник виду: {1: “Monday”, 2:...
 # Також в один рядок або як вдасться створити зворотний словник {“Monday”: 1,
+
+week_list = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+dict_first = {item[0]+1: item[1] for item in enumerate(week_list)}
+
+print(dict_first)
+
+dict_second = {item[1]: item[0]+1 for item in enumerate(week_list)}
+
+print(dict_second)
