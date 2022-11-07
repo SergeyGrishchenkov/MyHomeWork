f2761f43afeaa855865fd553b04e60efba49ce29
grisv <grisv1967@gmail.com>
2022-11-07 20:19:18 +0300

    Lesson 7 Home tasks

--- Practic/Lesson_7/ClassRoomTask.py
+++ Practic/Lesson_7/ClassRoomTask.py
@@ -1,2 +1,15 @@
 #list compre
 str_c = [1, 3, 2, 5, 6]
+
+i = 0
+while i < len(str_c):
+
+# for i in range(len(str_c)):
+    if str_c[i] % 3 == 0:
+        i += 1
+        continue
+    str_c[i] *= 5
+    i += 1
+else:
+    print('JR')
+print(str_c)
