5712398effe3826cd449e7b96f44f6014d078867
grisv <grisv1967@gmail.com>
2022-11-09 09:01:31 +0300

    Lesson 8 Home tasks

--- Lesson_8/task_2.py
+++ Lesson_8/task_2.py
@@ -4,5 +4,10 @@
 # Then create a dictionary from those two, with ‘name’ as a key and ‘capital’ as a parameter.
 # Make the function print out the values of the dictionary to make sure that it works as intended.
 
+def make_country(country, capital):
+    our_dict = {country: capital}
+    return our_dict
 
 if __name__ == "__main__":
+    our_dict = make_country('Ukraine', 'Kiev')
+    print(our_dict)
