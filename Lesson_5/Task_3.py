d17fea479ed2ff4d7d574a06f6f44f956b1cc537
grisv <grisv1967@gmail.com>
2022-11-10 12:21:23 +0300

    Lesson 5 Home tasks

--- Lesson_5/Task_3.py
+++ Lesson_5/Task_3.py
@@ -5,23 +5,25 @@
 # For example, the program obtained the word ‘hello’, so it should print 5 random strings(words) that combine characters
 from random import randint
 
-word_for_combination = input('Input any word. Pay attention, your word have to contain, at least, five characters.\n')
+test_case_tuple = ('always', 'forget', 'carefully', 'analyse', 'tasks')
 
-if len(word_for_combination) < 5:
-    print('Your word is not correct!')
-else:
-    # тут рандомно генерим символы из диапазона 0 - len(word_for_combination) -1
-    # самая большая сложность - отслеживать повторную генерацию символа
-    # Применим алгоритм контроля использованных индексов символов в списке
-    # Проверку на совпадение текущего и сгенерированного символа не делаем. В принципе, это не сдложно, конструкция if будет выглядеть так:
-    # 'if random_index not in list_used_symbols and random_index != index'
-    list_used_symbols = []
-    word_result = ''
-    index = 0
-    while index < len(word_for_combination):
-        random_index = randint(0, len(word_for_combination) - 1)
-        if random_index not in list_used_symbols:
-            word_result += word_for_combination[random_index]
-            list_used_symbols.append(random_index)
-            index += 1
-    print(f'We have such result: {word_result}')
+for item in test_case_tuple:
+    if len(item) < 5:
+        print('Your word is not correct!')
+    else:
+        # тут рандомно генерим символы из диапазона 0 - len(word_for_combination) -1
+        # самая большая сложность - отслеживать повторную генерацию символа
+        # Применим алгоритм контроля использованных индексов символов в списке
+        # Проверку на совпадение текущего и сгенерированного символа не делаем. В принципе, это не сдложно, конструкция if будет выглядеть так:
+        # 'if random_index not in list_used_symbols and random_index != index'
+        #list_used_symbols = [] # BETTER WAY TO USE TUPLE
+        list_used_symbols = []
+        word_result = ''
+        index = 0
+        while index < len(item):
+            random_index = randint(0, len(item) - 1)
+            if random_index not in list_used_symbols:
+                word_result += item[random_index]
+                list_used_symbols.append(random_index)
+                index += 1
+        print(f'For word: {item}, We have such result: {word_result}')
