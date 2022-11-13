d256f4e266c6074f67f0cf47633cf6fcc8ecb54d
grisv <grisv1967@gmail.com>
2022-11-13 17:54:19 +0300

    Lesson 10 Home tasks

--- lesson_10/task_2.py
+++ lesson_10/task_2.py
@@ -4,28 +4,26 @@
 # if the two values given by the input function were not numbers,
 # and if value b was zero (cannot divide by zero).
 
-def sqw_divide_number(a, b):
+def sqw_divide_number():
     successfully = False
+    first_arg = input("Input first number:\n")
+    second_arg = input("Input second number:\n")
     try:
-        result = (float(a) ** 2) / float(b)
+        if not first_arg.isdigit() or not second_arg.isdigit():
+            raise TypeError('Input arguments are not digit.')
+        result = (int(first_arg) ** 2) / int(second_arg)
         successfully = True
         return result
     except ZeroDivisionError:
-        #print(ZeroDivisionError.__doc__)
-        print('')
+        print(ZeroDivisionError.__doc__)
     except TypeError:
-        #print(TypeError.__doc__)
-        print('')
-    except:
-        print('--')
+        print(TypeError.__doc__)
     finally:
         if not successfully:
             print('You should be more careful when entering the value!!!')
 
 def main():
-    first_arg = input("Input first number:\n")
-    second_arg = input("Input second number:\n")
-    result = sqw_divide_number(first_arg, second_arg)
+    result = sqw_divide_number()
     print(f'You have such result: {result}')
 
 if __name__ == '__main__':
