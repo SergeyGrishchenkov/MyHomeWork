86c8c2ec6508b8a3a16b723819290afacfb56c24
grisv <grisv1967@gmail.com>
2022-11-27 14:08:43 +0300

    Lesson 13 Home tasks

--- lesson_13/task_3.py
+++ lesson_13/task_3.py
@@ -3,8 +3,14 @@
 # Otherwise, return the result of the second one
 
 def choose_func(nums: list, func1, func2):
-    pass
-
+    try:
+        if len(list(filter(lambda x: x > 0, nums))) == len(nums):
+            return func1(nums)
+        else:
+            return func2(nums)
+    except:
+        print(Exception.__doc__)
+        return Exception.__name__
 
 # Assertions
 
@@ -12,15 +18,20 @@ nums1 = [1, 2, 3, 4, 5]
 
 nums2 = [1, -2, 3, -4, 5]
 
-
 def square_nums(nums):
     return [num ** 2 for num in nums]
-
 
 def remove_negatives(nums):
     return [num for num in nums if num > 0]
 
+def main():
+    assert choose_func(nums1, square_nums, remove_negatives) == [1, 4, 9, 16, 25], 'Bad work for first condition!!!'
+    print(f'Good work for first condition!!!')
+    assert choose_func(nums2, square_nums, remove_negatives) == [1, 3, 5], 'Bad work for second condition!!!'
+    print(f'Good work for second condition!!!')
 
-assert choose_func(nums1, square_nums, remove_negatives) == [1, 4, 9, 16, 25]
-
-assert choose_func(nums2, square_nums, remove_negatives) == [1, 3, 5]
+if __name__ == '__main__':
+    main()
+# assert choose_func(nums1, square_nums, remove_negatives) == [1, 4, 9, 16, 25]
+#
+# assert choose_func(nums2, square_nums, remove_negatives) == [1, 3, 5]
