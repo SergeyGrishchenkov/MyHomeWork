e9d37f903358fce083663711bffcdc3533ff9be0
grisv <grisv1967@gmail.com>
2022-11-28 18:35:23 +0300

    Lesson 13 Home tasks

--- lesson_11/task_2.py
+++ lesson_11/task_2.py
@@ -3,7 +3,18 @@ import sys
 
 # ----
 def add_new(item: dict):
-    print('')
+    new_fname = inpup('Type on the keyboard the first name:\n')
+    new_lname = inpup('Type on the keyboard the last name:\n')
+    new_phone_number = inpup('Type on the keyboard the phone number:\n')
+    new_state = inpup('Type on the keyboard the State:\n')
+    new_city = inpup('Type on the keyboard the City:\n')
+    new_person = {'first_name': new_fname,
+                  'last_name': new_lname,
+                  'full_name': new_fname + ' ' + new_lname,
+                  'phone_numbers': [new_phone_number],
+                  'state': new_state,
+                  'city': new_city}
+    item.update(new_person)
 
 def find_by_first_name(name: str, book: list):
     #book.
@@ -29,7 +40,7 @@ def update_by_phonenumber():
 
 t_activity = (('Add new entries', 'Search by first name'), ('Add', 'fn'), ('Add new entries',add_new, find_by_first_name))
 
-possible_activity = {'add': ['Add new entries', add_new]}
+possible_activity = {'add': ['Add new entries', add_new], 'fn': ['Search by first name', find_by_first_name]}
 
 def start():
     choise = input("Would you like to work with the phone book?\nType Y - to start work or any key - to refuse:\n")
@@ -41,8 +52,12 @@ def start():
         case _:
             return False
 
-def choise_activity():
-    print('-------')
+def secect_action(actions: dict):
+    options_list = 'What do you want to do?\nPossible options:\n'
+    for k, v in actions.items():
+        options_list += v[0] + ' , to use, type on the keyboard : ' + k + '\n'
+    client_choice = input(options_list)
+    return client_choice
 
 #def main():
 start_working = start()
@@ -53,7 +68,9 @@ try:
     with open(name + ".json", "r", encoding='UTF-8') as book:
         our_pb = j.load(book)
         #choise of activity
-        print(type(our_pb))
+        type_activity = secect_action(possible_activity)
+
+        print(type(possible_activity[type_activity][1](our_pb)))
 
             #print(type(j.load(book)))
             #book_dic = j.load(book)
--- lesson_11/task_2_primary_data_filling.py
+++ lesson_11/task_2_primary_data_filling.py
@@ -1,13 +1,14 @@
 import json as j
 ask = input('Do you really want to fill primary data?\nAll previous data will be cleared!!!\nInput Y if you agree or wny symbol if not\n')
-l_states = ['Kiev', 'Lviv', 'Vinniza', 'Chernigiv']
-l_scities = {'Kiev': ['Kiev', 'Chabany', 'Gatne'], 'Lviv': ['Lviv', 'Stry'], 'Vinniza': ['Vinniza'], 'Chernigiv': ['Chernigiv']}
-persona_dict = dict(first_name='', last_name='', full_name='', phone_numbers=[], state=-1, city=-1)
+# l_states = ['Kiev', 'Lviv', 'Vinniza', 'Chernigiv']
+# l_scities = {'Kiev': ['Kiev', 'Chabany', 'Gatne'], 'Lviv': ['Lviv', 'Striy'], 'Vinniza': ['Vinniza'], 'Chernigiv': ['Chernigiv']}
+persona_dict = dict(first_name='', last_name='', full_name='', phone_numbers=[], state='', city='')
 
 if ask == 'Y':
     # We fill in the primary data of dictionaries states and cities
     with open('PB.json', 'w', encoding='ASCII') as f:
-       my_phonebook = {'states': l_states, 'cities': l_scities, 'persons': [persona_dict]}
+       # my_phonebook = {'states': l_states, 'cities': l_scities, 'persons': [persona_dict]}
+       my_phonebook = {'persons': [persona_dict]}
        j.dump(my_phonebook, f)
 
 
