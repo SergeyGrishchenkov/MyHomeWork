453f615a1f857a3597d1864e9dde99f07a33158e
grisv <grisv1967@gmail.com>
2022-11-30 21:26:00 +0300

    Lesson 11 Home tasks

--- lesson_11/task_2.py
+++ lesson_11/task_2.py
@@ -3,19 +3,24 @@ import sys
 
 # ----
 def add_new(item: dict):
-    new_fname = input('Type on the keyboard the first name:\n')
-    new_lname = input('Type on the keyboard the last name:\n')
-    new_phone_number = input('Type on the keyboard the phone number:\n')
-    new_state = input('Type on the keyboard the State:\n')
-    new_city = input('Type on the keyboard the City:\n')
-    new_person = {'first_name': new_fname,
-                  'last_name': new_lname,
-                  'full_name': new_fname + ' ' + new_lname,
-                  'phone_numbers': [new_phone_number],
-                  'state': new_state,
-                  'city': new_city}
-    item.update(new_person)
-
+    try:
+        new_fname = input('Type on the keyboard the first name:\n')
+        new_lname = input('Type on the keyboard the last name:\n')
+        new_phone_number = input('Type on the keyboard the phone number:\n')
+        new_state = input('Type on the keyboard the State:\n')
+        new_city = input('Type on the keyboard the City:\n')
+        new_person = {'first_name': new_fname,
+                      'last_name': new_lname,
+                      'full_name': new_fname + ' ' + new_lname,
+                      'phone_numbers': [new_phone_number],
+                      'state': new_state,
+                      'city': new_city}
+        item.update(new_person)
+        print('Information was successfully added!')
+        return True
+    except Exception:
+        ptint(f'Something was wrong with adding information\n{Exception.__doc__}')
+        return False
 def find_by_first_name(name: str, book: list):
     #book.
     print('')
@@ -38,9 +43,24 @@ def delete_by_phonenumber():
 def update_by_phonenumber():
         print('')
 
-t_activity = (('Add new entries', 'Search by first name'), ('Add', 'fn'), ('Add new entries',add_new, find_by_first_name))
+def exist_pb(actions: dict):
+    try:
+        with open(file_name, 'w') as f:
+            j.dump(actions, f)
+        sys.exit()
+    except Exception:
+        ptint(f'Something was wrong with adding information\n{Exception.__doc__}')
+        return False
 
-possible_activity = {'add': ['Add new entries', add_new], 'fn': ['Search by first name', find_by_first_name]}
+possible_activity = {'add': ['Add new entries', add_new],
+                    'fn': ['Search by first name', find_by_first_name],
+                    'fln': ['Search by last name', find_by_last_name],
+                    'ffn': ['Search by full name', find_by_full_name],
+                    'phone': ['Search by telephone number', find_by_phonenumber],
+                    'city': ['Search by city or state', find_by_city_state],
+                    'del': ['Delete a record for a given telephone number', delete_by_phonenumber],
+                    'upd': ['Update a record for a given telephone number', update_by_phonenumber],
+                    'ext': ['Exist the program', exist_pb]}
 
 def start():
     choise = input("Would you like to work with the phone book?\nType Y - to start work or any key - to refuse:\n")
@@ -52,6 +72,7 @@ def start():
         case _:
             return False
 
+
 def secect_action(actions: dict):
     options_list = 'What do you want to do?\nPossible options:\n'
     for k, v in actions.items():
@@ -59,31 +80,31 @@ def secect_action(actions: dict):
     client_choice = input(options_list)
     return client_choice
 
-#def main():
-start_working = start()
-if not start_working:
-    sys.exit()
-name = input("Input Phone book name:\n")
-try:
-    with open(name + ".json", "r", encoding='UTF-8') as book:
-        our_pb = j.load(book)
-        #choise of activity
-    type_activity = secect_action(possible_activity)
+def main():
+    start_working = start()
+    if not start_working:
+        sys.exit()
+    name = input("Input Phone book name:\n")
+    try:
+        with open(name + ".json", "r", encoding='UTF-8') as book:
+            our_pb = j.load(book)
+            #choise of activity
+        type_activity = secect_action(possible_activity)
 
-    possible_activity[type_activity][1](our_pb)
+        possible_activity[type_activity][1](our_pb)
 
-    with open(name + ".json", "w", encoding='UTF-8') as book:
-        j.dump(book, our_pb)
-        #print(type(j.load(book)))
-            #book_dic = j.load(book)
-            #activity = choise_activity()
-except FileNotFoundError:
-    print('Unfortunately, there are not such file!')
-    print(FileNotFoundError.__doc__)
-except:
-    print(Exception.__doc__)
+        with open(name + ".json", "w", encoding='UTF-8') as book:
+            j.dump(our_pb, book)
+            #print(type(j.load(book)))
+                #book_dic = j.load(book)
+                #activity = choise_activity()
+    except FileNotFoundError:
+        print('Unfortunately, there are not such file!')
+        print(FileNotFoundError.__doc__)
+    except:
+        print(Exception.__doc__)
 
 
 if __name__ == '__main__':
-    print('1')
+    main()
 
