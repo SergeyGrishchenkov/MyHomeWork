3fc6137e214f35eaec63b191de2950d14c1ebb89
grisv <grisv1967@gmail.com>
2022-12-09 09:18:34 +0300

    Lesson 16 Home tasks

--- lesson_16/task_3.py
+++ lesson_16/task_3.py
@@ -26,35 +26,67 @@ class Product:
 class ProductStore:
     __price_premium = 30
 
-    def __init__(self, address: str, income: int = 0, products: object = {}):
+    def __init__(self, address: str, income: int = 0, products: object = []):
         self.address = address
         self.income = income
         self.products = products
 
     def add(self, product: Product, amount: float):
         try:
-            product.price *= 1.3
-            if product.type in self.products.keys():
-                self.products[product.type].append(product)
-            else:
-                self.products[product.type] = [product]
+            product.price *= ((self.__price_premium / 100) + 1)
+            product.amount = amount
+            self.products.append(product)
         except:
             raise ValueError("Impossible to add priduct!")
 
     def set_discount(self, identifier, percent, identifier_type=''):
         try:
-            product.price *= 1.3
+            pass
+        except:
+            raise ValueError("Impossible to set discount!")
+
+    def sell_product(self, product_name, amount):
+        try:
+            pass
+        except:
+            raise ValueError("Impossible to set discount!")
+
+    def get_income(self):
+        try:
+            pass
+        except:
+            raise ValueError("Impossible to set discount!")
+
+    def get_product_info(self, product_name):
+        try:
+            pass
+        except:
+            raise ValueError("Impossible to set discount!")
+
+    def get_all_products(self):
+        try:
+            pass
         except:
             raise ValueError("Impossible to set discount!")
 
 
 p = Product('Sport', 'Football T-Shirt', 100)
 #
-# p2 = Product(Food, 'Ramen, 1.5)
+p2 = Product('Food', 'Ramen', 1.5)
 #
-s = ProductStore('sadghj')
+stor = ProductStore('sadghj')
 #
-s.add(p, 10)
+stor.add(p, 10)
+stor.add(p2, 220)
+aa = stor.products.copy()
+for item in stor.products:
+    print(type(item))
+    if 'Food' in item.__dict__.values():
+        print(item.__dict__)
+
+list_a = [-2, -1, 0, 1, 2, 3, 4, 5]    # Пусть у нас есть исходный список
+list_b = [x.__dict__ for x in stor.products]
+print(list_b)
 #
 # s.add(p2, 300)
 #
