966a2ad25f363da06b3251d09d985d79c9e9fca1
grisv <grisv1967@gmail.com>
2022-12-10 23:09:47 +0300

    Lesson 17 Home tasks

--- lesson_17/task_3.py
+++ lesson_17/task_3.py
@@ -7,13 +7,31 @@ class Fraction:
         self.nominator = nominator
         self.denominator = denominator
 
+    def reduction(self):
+        den = min(self.nominator, self.denominator) // 2
+
+
+    def intermediate_calculation(self, other):
+        return [self.nominator * other.denominator,
+                other.nominator * self.denominator,
+                other.denominator * self.denominator,
+                (self.denominator if self.denominator < other.denominator else other.denominator)]
+
     def __add__(self, other):
-        nominator_1 = self.nominator * other.denominator
-        nominator_2 = other.nominator * self.denominator
-        denominator_common = other.denominator * self.denominator
-        return Fraction((nominator_1 + nominator_2) / (self.denominator if self.denominator < other.denominator else other.denominator), denominator_common / (self.denominator if self.denominator < other.denominator else other.denominator))
+        items = self.intermediate_calculation(other)
+        return Fraction((items[0] + items[1]) / items[3], items[2] / items[3])
+
+    def __sub__(self, other):
+        items = self.intermediate_calculation(other)
+        return Fraction((items[0] - items[1]) / items[3], items[2] / items[3])
+
+    def __mul__(self, other):
+        return Fraction()
+
+    def __truediv__(self, other):
+        pass
 
 f1 = Fraction(3, 8)
 f2 = Fraction(5, 4)
-f3 = f1 + f2
+f3 = f1 - f2
 print(f3.__dict__)
