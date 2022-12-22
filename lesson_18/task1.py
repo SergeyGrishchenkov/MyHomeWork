bde2281fc427ae2929a192d096a2350a5ff4d9f9
grisv <grisv1967@gmail.com>
2022-12-23 00:15:03 +0300

    Lesson 18 Home tasks

--- lesson_18/task1.py
+++ lesson_18/task1.py
@@ -1,46 +1,34 @@
 # Create a class method named `validate`, which should be called from the `__init__` method to validate parameter email,
 # passed to the constructor.
 # The logic inside the `validate` method could be to check if the passed email parameter is a valid email string.
-
+import re
 from lesson_16.task_1 import Person
 
 
 class EmailAddress:
+    __pattern = r'\b[0-9a-zA-Z._-]+@[0-9a-zA-Z.-]+\.[a-zA-Z.]{2,}\b'  # регулярное выражение для проверки валидности
 
     @classmethod
     def check_email(cls, email):
-        if not isinstance(email, str):
-            raise Exception("Wrong type of email, should be string")
-        # далее проверяем на 5 признаков
-        # 1. наличие @
-        # 2. после символа @ есть символ .
-        # 3. после символа @ блоки, разделенные ".", имеют только сиволы буквы и цифры
-        # 4. последний блок после "." имеет только буквенное представление
-        state = True
-        s = email.strip().partition('@')
-        if s[1] != '@':
-            state = False
-        else:
-            s1 = s[2].split('.')
-            if len(s1) <= 1:
-                state = False
-            else:
-                counter = 0
-                while counter < len(s1):
-                    if counter == len(s1) - 1:
-                        state = s1[counter].isalnum()
-                    else:
-                        state = s1[counter].isalpha()
-                    counter += 1
-        if not state:
-            raise Exception("Wrong FORMAT of email!")
+        try:
+            if re.fullmatch(cls.__pattern, email) is None:
+                raise Exception
+            print('Email address format is CORRECT!!!')
+            return True
+        except:
+            print('Wrong email address format')
+            return False
+
+
 
     def __init__(self, person, email):
-        EmailAddress.check_email(email)
+        email_correct = EmailAddress.check_email(email)
         self.person = person
-        self.email = email.strip()
+        self.email = email.strip() if email_correct else ''
 
 
 if __name__ == '__main__':
     test1 = EmailAddress(Person('Anna', 'Samon', 'female', 25), 'test@eee.com')
     test2 = EmailAddress(Person('Anna', 'Samon', 'female', 25), 'testeee.com')
+    test3 = EmailAddress(Person('Anna', 'Samon', 'female', 25), 'test@eee.ccc.com')
+    test4 = EmailAddress(Person('Anna', 'Samon', 'female', 25), 'te[st@eee.ccc.com')
